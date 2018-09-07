4a01ebddc815efa571ce1c377de7c547442e7b36
Cesar TL <cesartronlozai@convendia.com>
2018-09-07 11:36:45 +0100

    Refactored SynonymService in a more functional way

--- src/main/java/com/e4developer/synonymsservice/SynonymsService.java
+++ src/main/java/com/e4developer/synonymsservice/SynonymsService.java
@@ -4,34 +4,32 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 @Service
 public class SynonymsService {
 
     private WebClient client = WebClient.create("https://api.datamuse.com/");
 
     public Mono<String> getSynonym(String word) {
-        Mono<SynonymResult[]> synonymResultsMono = client.get().uri("words?rel_syn="+word).retrieve().bodyToMono(SynonymResult[].class);
+        final Mono<SynonymResult[]> synonymResultsMono = client.get().uri("words?rel_syn=" + word).retrieve().bodyToMono(SynonymResult[].class)
+                .delayElement(Duration.ofSeconds(1)); //introducing small delay to show concurrency
         return synonymResultsMono.map(synonymResultList -> getBestSynonym(synonymResultList, word));
     }
 
     public Mono<String> getSynonymSentence(String sentence) {
-        String[] split = sentence.split(" ");
-        Mono<String> synSentence = Mono.just("");
-        for(String word : split){
-            synSentence = synSentence.zipWith(getSynonym(word), (w1, w2) -> w1 + " " +w2);
-        }
-        return synSentence;
+        return Stream.of(sentence.split(" "))
+                .map(this::getSynonym) //here we have a stream of Mono
+                .reduce(Mono.just(""), (m1, m2) -> m1.zipWith(m2, (w1, w2) -> w1 + " " + w2)); //we combine the mono with zipWith
     }
 
-    public String getBestSynonym(SynonymResult[] synonymResultList, String word){
-        int topScore = 0;
-        String topWord = word;
-        for(SynonymResult result : synonymResultList){
-            if(result.getScore() > topScore && !result.getWord().contains(word) && !result.getWord().contains(" ") ){
-                topScore = result.getScore();
-                topWord = result.getWord();
-            }
-        }
-        return topWord;
+    private String getBestSynonym(SynonymResult[] synonymResultList, String word) {
+        return Stream.of(synonymResultList)
+                .filter(r -> !r.getWord().equals(word) && !r.getWord().equals(""))
+                .max(Comparator.comparing(SynonymResult::getScore))
+                .map(SynonymResult::getWord)
+                .orElse(word); //if no synonym found we return the word
     }
 }
